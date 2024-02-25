0ab2f4a1ae01ca46777656a56c520f088cc3dc77
osistlk <osistlk@gmail.com>
2024-02-25 13:19:29 -0500

    Increase maxParallel to 100 for faster processing

--- src/lib/keyframes.js
+++ src/lib/keyframes.js
@@ -38,7 +38,7 @@ async function extractKeyframesForDirectory(directory) {
       }
     });
 
-    const maxParallel = 4;
+    const maxParallel = 100;
     for (let i = 0; i < tasks.length; i += maxParallel) {
       const batch = tasks.slice(i, i + maxParallel).map((task) => task());
       await Promise.all(batch);
