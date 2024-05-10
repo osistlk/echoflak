c040c5a48820d73f2c368b5467b1e1f8982f3935
osistlk <osistlk@gmail.com>
2024-05-10 17:20:15 -0400

    more functions

--- src/index.js
+++ src/index.js
@@ -92,138 +92,190 @@ async function runBatch() {
 }
 
 async function main() {
+  // Run each video keyframe extraction task
+  // I've done it this way so I can block n number of child processes until they finish
+  // Surely there will be no unintended consequences :D
   await runBatch();
-
-  const keyframesDir = `${inputDir}\\keyframes`;
-  // Check if the keyframe dir exists
-  if (!fs.existsSync(inputDir)) fs.mkdirSync(inputDir);
-
+  const keyframesDir = validateKeyframes();
   // Get all video directories within the specified directory
-  let videoDirs = fs
-    .readdirSync(keyframesDir)
-    .filter((file) => fs.statSync(path.join(keyframesDir, file)).isDirectory());
-  const videoHashes = {};
-  const duplicatesMap = {};
-
+  let { videoDirs, videoHashes, duplicatesMap } = prepProcessor();
   // Progress display initialization
-  let processed = 0;
-  const total = videoDirs.length;
-  console.log("");
-  console.log("\x1b[36m%s\x1b[0m", `Processing ${total} directories...`);
-
-  for (const videoDir of videoDirs) {
-    // Process each video directory to extract keyframes and generate hashes
-    const keyframeDir = path.join(keyframesDir, videoDir);
-    const keyframeFiles = fs
-      .readdirSync(keyframeDir)
-      .filter((file) => path.extname(file) === ".jpg");
-    videoHashes[videoDir] = [];
-
-    for (const file of keyframeFiles) {
-      // Generate and store perceptual hash for each keyframe
-      const hash = await generatePerceptualHash(path.join(keyframeDir, file));
-      videoHashes[videoDir].push(hash);
-    }
-
-    // Update progress for each processed directory
-    processed++;
-    // eslint-disable-next-line no-undef
-    process.stdout.write(
-      `\r\x1b[35mProgress: ${processed}/${total} directories processed.\x1b[0m`,
-    );
-  }
-
+  await generateKeyframeHashes();
   // Compare hashes of all videos to find duplicates
-  videoDirs.forEach((videoDir, index) => {
-    duplicatesMap[videoDir] = [];
-    for (let i = 0; i < videoDirs.length; i++) {
-      if (i !== index) {
-        const otherVideoDir = videoDirs[i];
-        if (
-          compareHashSets(videoHashes[videoDir], videoHashes[otherVideoDir])
-        ) {
-          // If duplicates are found, store them in the map
-          duplicatesMap[videoDir].push(otherVideoDir);
-          // remove the duplicate from the videoDirs array to avoid redundant comparisons
-          videoDirs.splice(i, 1);
-          i--;
-        }
-      }
-    }
-  });
-  fs.writeFileSync("duplicates.json", JSON.stringify(duplicatesMap, null, 2));
-
+  findDuplicates();
   // Read duplicate data from JSON file
-  const duplicatesData = JSON.parse(fs.readFileSync("duplicates.json", "utf8"));
-  const duplicatesDir = path.join(inputDir, "duplicates");
+  moveDuplicates();
+  // Filter MP4 files that haven't been moved to duplicates
+  mergeAndOutputVideo();
+  // Move duplicates back to original folder
+  moveDuplicatesBack();
+  cleanup();
+  goodbye();
 
-  // Create the duplicates directory if it doesn't exist
-  if (!fs.existsSync(duplicatesDir)) {
-    fs.mkdirSync(duplicatesDir, { recursive: true });
-  }
-
-  // A set to keep track of videos that have been moved, ensuring no original is moved
-  const movedVideos = new Set();
-  videoDirs = Object.keys(duplicatesData);
-
-  videoDirs.forEach((videoDir) => {
-    const duplicateDirs = duplicatesData[videoDir];
-
-    duplicateDirs.forEach((dupDir) => {
-      const originalPath = path.join(inputDir, dupDir) + ".mp4";
-      const targetPath = path.join(duplicatesDir, dupDir) + ".mp4";
-      if (fs.existsSync(originalPath)) {
-        fs.renameSync(originalPath, targetPath);
-        console.log();
-        console.log(`Moved ${dupDir} to duplicates.`);
-        // Mark this video as moved to avoid considering it as original in future
-        movedVideos.add(dupDir);
+  function moveDuplicatesBack() {
+    fs.readdirSync("duplicates").forEach((file) => {
+      const sourcePath = path.join(`${inputDir}\\duplicates`, file);
+      const destPath = path.join(inputDir, file);
+      try {
+        fs.renameSync(sourcePath, destPath);
+        console.log(`Moved ${file} to ${inputDir}`);
+      } catch {
+        console.log("Error when moving duplicates back to source directory.");
       }
     });
-  });
-
-  console.log();
-  console.log("Duplicates moved.");
-
-  // Filter MP4 files that haven't been moved to duplicates
-  const uniqueVideoFiles = fs
-    .readdirSync(inputDir)
-    .filter(
-      (file) =>
-        path.extname(file) === ".mp4" &&
-        !fs.existsSync(path.join(inputDir, "duplicates", file)),
-    );
-  const fileListPath = path.join(inputDir, "filelist.txt");
-  // Create content for the file list in ffmpeg's required format
-  const fileContent = uniqueVideoFiles.map((file) => `file '${file}'`).join("\n");
-  fs.writeFileSync(fileListPath, fileContent);
-
-  const outputFilePath = path.join(
-    config.outputDir,
-    `${config.outputFilename}.mp4`,
-  );
-
-  // ffmpeg command to concatenate videos listed in the file list
-  const command = `ffmpeg -y -f concat -safe 0 -i "${fileListPath}" -c copy "${outputFilePath}"`;
-  child_process.execSync(command);
-  console.log("Videos have been concatenated successfully.");
-
-  console.log();
-  console.timeEnd("Execution time");
-
-  try {
-    if (config.clean.logs) fs.rmSync(`${inputDir}\\filelist.txt`);
-    if (config.clean.keyframes)
-      fs.rmSync(`${keyframesDir}`, { recursive: true, force: true });
-  } catch {
-    console.log("Error during cleanup.");
   }
 
-  console.log(
-    "\x1b[33m%s\x1b[0m",
-    "Thank you for using the duplicate detection script!",
-  );
-  console.log("Goodbye.");
+  function goodbye() {
+    console.log(
+      "\x1b[33m%s\x1b[0m",
+      "Thank you for using the duplicate detection script!",
+    );
+    console.log("Goodbye.");
+  }
+
+  function cleanup() {
+    try {
+      if (config.clean.logs) fs.rmSync(`${inputDir}\\filelist.txt`);
+      if (config.clean.keyframes)
+        fs.rmSync(`${keyframesDir}`, { recursive: true, force: true });
+      if (config.clean.duplicates)
+        fs.rmSync(`${inputDir}\\duplicates`, { recursive: true, force: true });
+    } catch {
+      console.log("Error during cleanup.");
+    }
+  }
+
+  function mergeAndOutputVideo() {
+    const uniqueVideoFiles = fs
+      .readdirSync(inputDir)
+      .filter(
+        (file) =>
+          path.extname(file) === ".mp4" &&
+          !fs.existsSync(path.join(inputDir, "duplicates", file)),
+      );
+    const fileListPath = path.join(inputDir, "filelist.txt");
+    // Create content for the file list in ffmpeg's required format
+    const fileContent = uniqueVideoFiles
+      .map((file) => `file '${file}'`)
+      .join("\n");
+    fs.writeFileSync(fileListPath, fileContent);
+
+    const outputFilePath = path.join(
+      config.outputDir,
+      `${config.outputFilename}.mp4`,
+    );
+
+    // ffmpeg command to concatenate videos listed in the file list
+    const command = `ffmpeg -y -f concat -safe 0 -i "${fileListPath}" -c copy "${outputFilePath}"`;
+    child_process.execSync(command);
+    console.log("Videos have been concatenated successfully.");
+
+    console.log();
+    console.timeEnd("Execution time");
+  }
+
+  function moveDuplicates() {
+    const duplicatesData = JSON.parse(
+      fs.readFileSync("duplicates.json", "utf8"),
+    );
+    const duplicatesDir = path.join(inputDir, "duplicates");
+
+    // Create the duplicates directory if it doesn't exist
+    if (!fs.existsSync(duplicatesDir)) {
+      fs.mkdirSync(duplicatesDir, { recursive: true });
+    }
+
+    // A set to keep track of videos that have been moved, ensuring no original is moved
+    const movedVideos = new Set();
+    videoDirs = Object.keys(duplicatesData);
+
+    videoDirs.forEach((videoDir) => {
+      const duplicateDirs = duplicatesData[videoDir];
+
+      duplicateDirs.forEach((dupDir) => {
+        const originalPath = path.join(inputDir, dupDir) + ".mp4";
+        const targetPath = path.join(duplicatesDir, dupDir) + ".mp4";
+        if (fs.existsSync(originalPath)) {
+          fs.renameSync(originalPath, targetPath);
+          console.log();
+          console.log(`Moved ${dupDir} to duplicates.`);
+          // Mark this video as moved to avoid considering it as original in future
+          movedVideos.add(dupDir);
+        }
+      });
+    });
+
+    console.log();
+    console.log("Duplicates moved.");
+  }
+
+  function findDuplicates() {
+    videoDirs.forEach((videoDir, index) => {
+      duplicatesMap[videoDir] = [];
+      for (let i = 0; i < videoDirs.length; i++) {
+        if (i !== index) {
+          const otherVideoDir = videoDirs[i];
+          if (
+            compareHashSets(videoHashes[videoDir], videoHashes[otherVideoDir])
+          ) {
+            // If duplicates are found, store them in the map
+            duplicatesMap[videoDir].push(otherVideoDir);
+            // remove the duplicate from the videoDirs array to avoid redundant comparisons
+            videoDirs.splice(i, 1);
+            i--;
+          }
+        }
+      }
+    });
+    fs.writeFileSync("duplicates.json", JSON.stringify(duplicatesMap, null, 2));
+  }
+
+  async function generateKeyframeHashes() {
+    let processed = 0;
+    const total = videoDirs.length;
+    console.log("");
+    console.log("\x1b[36m%s\x1b[0m", `Processing ${total} directories...`);
+
+    for (const videoDir of videoDirs) {
+      // Process each video directory to extract keyframes and generate hashes
+      const keyframeDir = path.join(keyframesDir, videoDir);
+      const keyframeFiles = fs
+        .readdirSync(keyframeDir)
+        .filter((file) => path.extname(file) === ".jpg");
+      videoHashes[videoDir] = [];
+
+      for (const file of keyframeFiles) {
+        // Generate and store perceptual hash for each keyframe
+        const hash = await generatePerceptualHash(path.join(keyframeDir, file));
+        videoHashes[videoDir].push(hash);
+      }
+
+      // Update progress for each processed directory
+      processed++;
+      // eslint-disable-next-line no-undef
+      process.stdout.write(
+        `\r\x1b[35mProgress: ${processed}/${total} directories processed.\x1b[0m`,
+      );
+    }
+  }
+
+  function prepProcessor() {
+    let videoDirs = fs
+      .readdirSync(keyframesDir)
+      .filter((file) =>
+        fs.statSync(path.join(keyframesDir, file)).isDirectory(),
+      );
+    const videoHashes = {};
+    const duplicatesMap = {};
+    return { videoDirs, videoHashes, duplicatesMap };
+  }
+
+  function validateKeyframes() {
+    const keyframesDir = `${inputDir}\\keyframes`;
+    // Check if the keyframe dir exists
+    if (!fs.existsSync(inputDir)) fs.mkdirSync(inputDir);
+    return keyframesDir;
+  }
 }
 
 greeting();
